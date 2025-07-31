014709e15de2ad0d24372428318977bd4b21f1f1
Snaarze <panlasiguijeremyglenn49@gmail.com>
2025-07-31 15:37:28 +0800

    added of creating new issue with hook forms

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,16 +1,48 @@
 "use client";
-import { TextField, Flex, Button } from "@radix-ui/themes";
-
-import SimpleMDE from "react-simplemde-editor";
+import { TextField, Button } from "@radix-ui/themes";
+import { useForm, Controller } from "react-hook-form";
 import "easymde/dist/easymde.min.css";
+import dynamic from "next/dynamic";
+import axios from "axios";
+import { useRouter } from "next/navigation";
+
+// for client side rendering
+const SimpleMDE = dynamic(() => import("react-simplemde-editor"), {
+  ssr: false,
+});
+
+interface IssueForm {
+  title: string;
+  description: string;
+}
 
 const page = () => {
+  const { register, control, handleSubmit } = useForm<IssueForm>();
+  const router = useRouter();
+  const onSubmit = async (data: IssueForm) => {
+    await axios.post("/api/issues/", data);
+    router.push("/issues");
+    alert("Successfully Added!");
+  };
+
   return (
-    <Flex direction="column" gap="3" className="max-w-xl">
-      <TextField.Root placeholder="Title" size="3"></TextField.Root>
-      <SimpleMDE placeholder="Description" />
-      <Button>Submit New Issue</Button>
-    </Flex>
+    <form action="" onSubmit={handleSubmit(onSubmit)}>
+      <TextField.Root
+        placeholder="Title"
+        size="3"
+        {...register("title")}
+      ></TextField.Root>
+      {/* if some libraries does not allow to directly modify the properties use controller  */}
+      <Controller
+        name="description"
+        control={control}
+        render={({ field }) => (
+          <SimpleMDE placeholder="Description" {...field} />
+        )}
+      />
+
+      <Button type="submit">Submit New Issue</Button>
+    </form>
   );
 };
 
