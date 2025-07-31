92031c6b320950d8681cbf4e573eafad4ce0fb5c
Snaarze <panlasiguijeremyglenn49@gmail.com>
2025-07-31 19:24:46 +0800

    added color for each status, created new component for reusability

---
+++ app/components/IssueStatusBadge.tsx
@@ -0,0 +1,24 @@
+import React, { PropsWithChildren } from "react";
+import { Status } from "../generated/prisma";
+import { Badge } from "@radix-ui/themes";
+
+interface Props {
+  status: Status;
+}
+
+const statusMap: Record<
+  Status,
+  { label: string; color: "red" | "violet" | "green" }
+> = {
+  OPEN: { label: "Open", color: "red" },
+  IN_PROGRESS: { label: "In Progress", color: "violet" },
+  CLOSED: { label: "In Progress", color: "green" },
+};
+
+const IssueStatusBadge = ({ status }: Props) => {
+  return (
+    <Badge color={statusMap[status].color}>{statusMap[status].label}</Badge>
+  );
+};
+
+export default IssueStatusBadge;
