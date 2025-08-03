820645ec4791dfeb2e64fccf67747e35a2a249f1
Snaarze <panlasiguijeremyglenn49@gmail.com>
2025-08-03 10:48:34 +0800

    edit issue component

--- app/issues/[id]/edit/page.tsx
+++ app/issues/[id]/edit/page.tsx
@@ -1,63 +1,22 @@
-"use client";
-import { ErrorMessage } from "@/app/components";
-import React, { useState } from "react";
-import "easymde/dist/easymde.min.css";
-import dynamic from "next/dynamic";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { issuesSchema } from "@/app/validationSchema";
-import { z } from "zod";
-import { useForm, Controller } from "react-hook-form";
-import { Button, Spinner, TextField } from "@radix-ui/themes";
+import React from "react";
+import IssueForm from "../../_components/issueForm";
+import { prisma } from "@/prisma/client";
+import { notFound } from "next/navigation";
 
-const SimpleMDE = dynamic(() => import("react-simplemde-editor"), {
-  ssr: false,
-});
 interface Props {
   params: { id: string };
 }
 
-type issueForm = z.infer<typeof issuesSchema>;
-
-const page = ({ params }: Props) => {
-  const [isSubmitted, setSubmitting] = useState(false);
-  const {
-    register,
-    control,
-    handleSubmit,
-    formState: { errors, isLoading },
-  } = useForm<issueForm>({
-    resolver: zodResolver(issuesSchema),
+const EditIssuePage = async ({ params }: Props) => {
+  const { id } = await params;
+  const issue = await prisma.issue.findUnique({
+    where: {
+      id: parseInt(id),
+    },
   });
 
-  const onSubmit = (data: issueForm) => console.log(data);
-
-  return (
-    <div>
-      <form action="" onSubmit={handleSubmit(onSubmit)}>
-        <TextField.Root
-          placeholder="Title"
-          size="3"
-          {...register("title")}
-        ></TextField.Root>
-        <ErrorMessage>{errors.title?.message}</ErrorMessage>
-
-        {/* if some libraries does not allow to directly modify the properties use controller  */}
-        <Controller
-          name="description"
-          control={control}
-          render={({ field }) => (
-            <SimpleMDE placeholder="Description" {...field} />
-          )}
-        />
-
-        <ErrorMessage>{errors.description?.message}</ErrorMessage>
-
-        <Button type="submit" disabled={isSubmitted}>
-          Submit New Issue {isSubmitted && <Spinner />}
-        </Button>
-      </form>
-    </div>
-  );
+  if (!issue) notFound();
+  return <IssueForm issue={issue} />;
 };
 
-export default page;
+export default EditIssuePage;
---
+++ app/issues/_components/issueForm.tsx
@@ -0,0 +1,92 @@
+"use client";
+import { TextField, Button, Callout, Text, Spinner } from "@radix-ui/themes";
+import { useForm, Controller } from "react-hook-form";
+import "easymde/dist/easymde.min.css";
+import dynamic from "next/dynamic";
+import axios from "axios";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { HiInformationCircle } from "react-icons/hi2";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { issuesSchema } from "@/app/validationSchema";
+import { z } from "zod";
+import ErrorMessage from "@/app/components/ErrorMessage";
+import { Issue } from "@/app/generated/prisma";
+// using _ following with the name folder will separate the implementation detail and exclude in the routing file system
+// for client side rendering
+const SimpleMDE = dynamic(() => import("react-simplemde-editor"), {
+  ssr: false,
+});
+
+interface Props {
+  issue?: Issue;
+}
+
+// this is a built in that make a interface with the help of defining of the schema
+type issueFormData = z.infer<typeof issuesSchema>;
+
+const IssueForm = ({ issue }: Props) => {
+  const [isSubmitted, setSubmitting] = useState(false);
+  const {
+    register,
+    control,
+    handleSubmit,
+    formState: { errors, isLoading },
+  } = useForm<issueFormData>({
+    resolver: zodResolver(issuesSchema),
+  });
+
+  const [error, setError] = useState("");
+  const router = useRouter();
+  const onSubmit = async (data: issueFormData) => {
+    try {
+      setSubmitting(true);
+      await axios.post("/api/issues/", data);
+      router.push("/issues");
+      alert("Successfully Added!");
+    } catch (e) {
+      setError("An unexpected Error");
+      setSubmitting(false);
+    }
+  };
+
+  return (
+    <div className="max-w-xl">
+      {error && (
+        <Callout.Root color="red" className="mb-5">
+          <Callout.Icon>
+            <HiInformationCircle />
+          </Callout.Icon>
+          <Callout.Text>{error}</Callout.Text>
+        </Callout.Root>
+      )}
+      <form action="" onSubmit={handleSubmit(onSubmit)}>
+        <TextField.Root
+          defaultValue={issue?.title}
+          placeholder="Title"
+          size="3"
+          {...register("title")}
+        ></TextField.Root>
+        <ErrorMessage>{errors.title?.message}</ErrorMessage>
+
+        {/* if some libraries does not allow to directly modify the properties use controller  */}
+        <Controller
+          defaultValue={issue?.description}
+          name="description"
+          control={control}
+          render={({ field }) => (
+            <SimpleMDE placeholder="Description" {...field} />
+          )}
+        />
+
+        <ErrorMessage>{errors.description?.message}</ErrorMessage>
+
+        <Button type="submit" disabled={isSubmitted}>
+          Submit New Issue {isSubmitted && <Spinner />}
+        </Button>
+      </form>
+    </div>
+  );
+};
+
+export default IssueForm;
