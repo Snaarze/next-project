6f9ec2e8007f7de8e081abbb10aacfe4a99fa111
Snaarze.jp <panlasiguijeremyglenn49.com>
2025-08-05 17:10:14 +0800

    build summary component

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,23 @@
+import { prisma } from "@/prisma/client";
+import IssueSummary from "./IssueSummary";
 import LatestIssues from "./LatestIssues";
 
 export default async function Home() {
-  return <LatestIssues />;
+  const open = await prisma.issue.count({
+    where: {
+      status: "OPEN",
+    },
+  });
+  const closed = await prisma.issue.count({
+    where: {
+      status: "CLOSED",
+    },
+  });
+  const inProgress = await prisma.issue.count({
+    where: {
+      status: "IN_PROGRESS",
+    },
+  });
+
+  return <IssueSummary open={open} inProgress={inProgress} closed={closed} />;
 }
