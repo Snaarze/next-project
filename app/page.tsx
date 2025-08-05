9d884ccc39a7c08a57b73e7fac04875b9c893841
Snaarze.jp <panlasiguijeremyglenn49.com>
2025-08-05 10:33:22 +0800

    build the layout of pagination

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,9 @@
+import Pagination from "./components/Pagination";
+
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <div>
+      <Pagination itemCount={100} pageSize={10} currentPage={1} />
+    </div>
+  );
 }
