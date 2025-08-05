8921df76a0c009f681d6473948039a30192d6399
Snaarze.jp <panlasiguijeremyglenn49.com>
2025-08-05 16:46:34 +0800

    build the latest component

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,5 @@
-import Pagination from "./components/Pagination";
+import LatestIssues from "./LatestIssues";
 
-type SearchParams = Promise<{ page: string }>;
-
-interface Props {
-  searchParams: SearchParams;
-}
-
-export default async function Home(props: Props) {
-  const searchParams = await props.searchParams;
-
-  return (
-    <div>
-      <Pagination
-        itemCount={100}
-        pageSize={10}
-        currentPage={parseInt(searchParams.page)}
-      />
-    </div>
-  );
+export default async function Home() {
+  return <LatestIssues />;
 }
